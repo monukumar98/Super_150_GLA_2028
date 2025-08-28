c59215476fb65954d9d3ef14b5ce21d528169380
monukumar98 <monu012as@gmail.com>
2025-08-28 16:55:51 +0530

    Array LeetCode

---
+++ Lec7/Reversal_Algo.java
@@ -0,0 +1,38 @@
+package Lec7;
+
+public class Reversal_Algo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+		int k = 3;
+		Rotate(arr, k);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public static void Rotate(int[] arr, int k) {
+		int n = arr.length;
+		k = k % n;
+		// staring ke n-k element reverse[0,n-k-1]
+		Reverse(arr, 0, n - k - 1);
+		// last ke k element reverse
+		Reverse(arr, n - k, n - 1);
+		// all element reverse
+		Reverse(arr, 0, n - 1);
+	}
+
+	public static void Reverse(int[] arr, int i, int j) {
+		// TODO Auto-generated method stub
+		while (i < j) {
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+
+	}
+
+}
