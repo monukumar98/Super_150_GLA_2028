4114cc7cf9975d6a8588fa014370f722fee02893
monukumar98 <monu012as@gmail.com>
2025-09-03 18:48:55 +0530

    Sorting kadanes Algo

--- Lec8/Kadanes_Algorithm.java
+++ Lec8/Kadanes_Algorithm.java
@@ -1,5 +1,23 @@
 package Lec8;
 
 public class Kadanes_Algorithm {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println(Maximum_Sum(arr));
+	}
 
+	public static int Maximum_Sum(int[] arr) {
+		int ans = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+			ans = Math.max(ans, sum);
+			if (sum < 0) {
+				sum = 0;
+			}
+		}
+		return ans;
+
+	}
 }
