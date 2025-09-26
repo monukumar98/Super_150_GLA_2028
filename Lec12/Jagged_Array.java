4d1558e65be08d4d4576a0b06d386f00be261bc2
monukumar98 <monu012as@gmail.com>
2025-09-26 15:20:56 +0530

    2D Array

---
+++ Lec12/Jagged_Array.java
@@ -0,0 +1,15 @@
+package Lec12;
+
+public class Jagged_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = new int[3][];
+
+		arr[0] = new int[4];
+		arr[1] = new int[2];
+		arr[2] = new int[3];
+
+	}
+
+}
