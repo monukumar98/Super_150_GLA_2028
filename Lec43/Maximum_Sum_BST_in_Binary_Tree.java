c56789d6e5dc2ee8a10365718d4cf92706b8b89d
monukumar98 <monu012as@gmail.com>
2026-08-26 12:42:47 +0530

    BST QPS

---
+++ Lec43/Maximum_Sum_BST_in_Binary_Tree.java
@@ -0,0 +1,59 @@
+package Lec43;
+
+import Lec43.Validate_Binary_Search_Tree.BstPair;
+import Lec43.Validate_Binary_Search_Tree.TreeNode;
+
+public class Maximum_Sum_BST_in_Binary_Tree {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	class Solution {
+		public int maxSumBST(TreeNode root) {
+			return ValidBST(root).ans;
+		}
+
+		public BstPair ValidBST(TreeNode root) {
+			if (root == null) {
+				return new BstPair();
+			}
+			BstPair lbp = ValidBST(root.left);
+			BstPair rbp = ValidBST(root.right);
+			BstPair sbp = new BstPair();
+			sbp.min = Math.min(lbp.min, Math.min(rbp.min, root.val));
+			sbp.max = Math.max(lbp.max, Math.max(rbp.max, root.val));
+			sbp.sum = lbp.sum + rbp.sum + root.val;
+			if (lbp.isbst && rbp.isbst && lbp.max < root.val && rbp.min > root.val) {
+				sbp.isbst = true;
+				sbp.ans = Math.max(lbp.ans, Math.max(rbp.ans, sbp.sum));
+			} else {
+				sbp.isbst = false;
+				sbp.ans = Math.max(lbp.ans, rbp.ans);
+			}
+			return sbp;
+		}
+	}
+
+	class BstPair {
+		boolean isbst = true;
+		long max = Long.MIN_VALUE;
+		long min = Long.MAX_VALUE;
+		int sum = 0;
+		int ans = 0;
+	}
+}
